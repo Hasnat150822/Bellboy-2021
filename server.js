9b6494c443dbd14752292c213681ad26ad6fa34d
Hasnat150822git config --global user.email hasnat150822@gmail.com <hasnat150822@gmail.com>
2021-06-02 12:37:27 +0500

    asdf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const app = express()
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
